cdd8a45e08455cf6070a1aacff326d0cebfed55b
fen666 <lf00817@163.com>
2017-07-27 13:06:33 +0800

    first commit

---
+++ example/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// import 'highlight.js/styles/monokai-sublime.css'
+import VueHighlightJS from 'vue-highlightjs'
+Vue.use(VueHighlightJS)
+
+import VueDemonstration from 'vue-demonstration'
+Vue.component('demonstration', VueDemonstration)
+
+import Rate from '../../src/index.js'
+Vue.component(Rate.name, Rate)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
